e5f9e7a165fa8746f8da89f993876a0e498c3fe5
HaMa <vanha.jungle@gmail.com>
2020-11-20 18:22:16 +0700

    change name

--- src/App.js
+++ src/App.js
@@ -6,28 +6,47 @@ import { render } from '@testing-library/react';
 class App extends Component {
 	state = {
 		people: [
-			{ name: 'Hien Pham', age: '20' },
-			{ name: 'Van Ha', age: '19' },
-			{ name: 'Long Pham', age: '20' },
+			{ id: 'user1', name: 'Hien Pham', age: '20' },
+			{ id: 'user2', name: 'Van Ha', age: '19' },
+			{ id: 'user3', name: 'Long Pham', age: '20' },
 		],
+		otherState: 'Some thing',
+		displayPerson: false
 	};
 
+	personsHandler = () => {
+		let disPerson = this.state.displayPerson;
+		this.setState({
+			displayPerson: !disPerson
+		})
+	}
+
+	changedNameHandler = (event, id) => {
+		const personId = this.state.people.findIndex((person) => {
+			return person.id === id;
+		});
+
+		let persons = [...this.state.people];
+		persons[personId].name = event.target.value;
+		this.setState({
+			people: persons
+		})
+
+	}
+
 	render() {
+		let { people, otherState, displayPerson } = this.state;
+
+		let persons = displayPerson === true ? (
+			people.map((person) => {
+				return <Person name={person.name} age={person.age} key={person.id} changed={ (event) => this.changedNameHandler(event, person.id)}/>
+			})
+		) : null
+
 		return (
 			<div className='App'>
-				<Person
-					name={this.state.people[0].name}
-					age={this.state.people[0].age}
-				/>
-				<Person
-					name={this.state.people[1].name}
-					age={this.state.people[1].age}>
-					My course: Develop for myself
-				</Person>
-				<Person
-					name={this.state.people[2].name}
-					age={this.state.people[2].age}
-				/>
+				<button onClick={this.personsHandler}>Click</button>
+				{persons}
 			</div>
 		);
 	}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
 const Person = (props) => {
+
+    let style = {
+        border: '1px solid #555',
+        borderRadius: '10px',
+        width: '30%',
+        padding: '10px',
+        margin: '20px auto',
+        boxShadow: '0 5px 15px -10px #929292'
+    }
+
     return (
         <div className="person">
-            <h3>I'm a {props.name} and I'm {props.age} year old</h3>
-            <i>{ props.children}</i>
+            <div style={style}>
+                <h3>I'm a {props.name} and I'm {props.age} year old</h3>
+                <input type="text" name="name" placeholder={props.name} onChange={ props.changed}/>
+            </div>
         </div>
     )
 }
